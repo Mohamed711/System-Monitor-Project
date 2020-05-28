e374ffdc20f4d105f15654fc0cff8cc902921e92
Mohamed Abbas <mohamed71992@gmail.com>
2020-05-28 21:41:39 +0200

    Processor class implementation

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,17 @@
 #include "processor.h"
+#include "linux_parser.h"
 
-// TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+// Return the aggregate CPU utilization
+float Processor::Utilization() {
+    long CurrectActiveJiffies = LinuxParser::ActiveJiffies();
+    long CurrectTotalJiffies = LinuxParser::Jiffies();
+
+    float AverageUtil = (CurrectActiveJiffies - PrevActiveJiffies) /
+                        (float)(CurrectTotalJiffies - PrevTotalJiffies);
+
+    // Update latest read values
+    PrevActiveJiffies = CurrectActiveJiffies;
+    PrevTotalJiffies = CurrectTotalJiffies;
+
+    return AverageUtil;
+}
