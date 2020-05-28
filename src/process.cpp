1b66d6b8ab9c6743a8221338a0206f3cacbffc02
Mohamed Abbas <mohamed71992@gmail.com>
2020-05-28 21:42:19 +0200

    Process services implementation

--- src/process.cpp
+++ src/process.cpp
@@ -4,30 +4,62 @@
 #include <string>
 #include <vector>
 
+#include "linux_parser.h"
 #include "process.h"
 
 using std::string;
 using std::to_string;
 using std::vector;
 
-// TODO: Return this process's ID
-int Process::Pid() { return 0; }
+// Return this process's ID
+int Process::Pid() const{
+    return pid_;
+}
 
-// TODO: Return this process's CPU utilization
-float Process::CpuUtilization() { return 0; }
+// Compute the process CPU utilization
+void Process::ComputeCpuUtil()
+{
+    // Process total running time in seconds
+    float CurrectProcessTotalTime = LinuxParser::ActiveJiffies(pid_)/(float)sysconf(_SC_CLK_TCK);
+  
+    // The proccess available time (seconds = uptime of processor - (proccess start time / Hertz)
+    float CurrectSeconds = LinuxParser::UpTime() - (UpTime()/(float)sysconf(_SC_CLK_TCK));
 
-// TODO: Return the command that generated this process
-string Process::Command() { return string(); }
+    // Process CPU Utilization = proccess running time / total time available for the process
+    CpuUtilValue = (CurrectProcessTotalTime - PrevProcessTotalTime)/
+                        (float) (CurrectSeconds - PrevSeconds);
 
-// TODO: Return this process's memory utilization
-string Process::Ram() { return string(); }
+    // Update latest read values
+    PrevProcessTotalTime = CurrectProcessTotalTime;
+    PrevSeconds = CurrectSeconds;
+}
 
-// TODO: Return the user (name) that generated this process
-string Process::User() { return string(); }
+// Return this process's CPU utilization
+float Process::CpuUtilization() const{
+    return CpuUtilValue;
+}
 
-// TODO: Return the age of this process (in seconds)
-long int Process::UpTime() { return 0; }
+// Return the command that generated this process
+string Process::Command() {
+    return LinuxParser::Command(pid_);
+}
 
-// TODO: Overload the "less than" comparison operator for Process objects
-// REMOVE: [[maybe_unused]] once you define the function
-bool Process::operator<(Process const& a[[maybe_unused]]) const { return true; }
+// Return this process's memory utilization
+string Process::Ram() {
+    return LinuxParser::Ram(pid_);
+}
+
+// Return the user (name) that generated this process
+string Process::User() {
+    return LinuxParser::User(pid_) ;
+}
+
+// Return the age of this process (in seconds)
+long int Process::UpTime() {
+    return LinuxParser::UpTime(pid_);
+}
+
+// Overload the "less than" comparison operator for Process objects
+bool Process::operator<(Process const& a) const {
+    return CpuUtilValue < a.CpuUtilization() ? false : true;
+}
